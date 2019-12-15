8956402f57b29d455020364833211d21dfcd73f3
tohidplus <tohid.dadashnejad@gmail.com>
2019-12-15 16:30:32 +0330

    Create a file for jdd function

---
+++ functions.php
@@ -0,0 +1,9 @@
+function jdd($data)
+{
+    $data = is_string($data) ? $data : json_encode($data,JSON_PRETTY_PRINT);
+    $dbgt = debug_backtrace();
+    echo "\e[42;1;37mJson dump info:\e[0m\n";
+    echo "\e[0;33m" . $dbgt[0]['file'] . ':' . $dbgt[0]['line'] . "\e[0m" . PHP_EOL;
+    echo "\e[1;36m" . $data . "\e[0m" . PHP_EOL.PHP_EOL;
+    die();
+}
